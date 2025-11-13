e7da49b4116108d2f5fdf0fdba069d5da7c9389f
sahadnishy-css <sahadnishy@gmail.com>
2025-11-13 22:51:32 +0530

    Add SQL script for inserting books and querying library

---
+++ day 2/homework.sql
@@ -0,0 +1,15 @@
+//add 5 books
+INSERT INTO library(title, author, price, genre)
+VALUES('Da vinci Code', 'Dan Brown', '450', 'fiction'),
+    ('The Great Gatsby', 'F Scott Fitzgerals', '350', 'fiction'),
+    ('Pride and Prejudice', 'Jane Austen', '200', 'science'),
+    ('Early India', 'Romila Thapar', '550', 'history'),
+    ('Vagabonding', 'Rolf Potts', '280', 'travelogue')
+//books with price more than 400
+SELECT * FROM library WHERE price > 400;
+// books in the genres science, history and fiction
+SELECT * FROM library WHERE genre IN ('science', 'history', 'fiction');
+// book with title The great gatsby
+SELECT * FROM library WHERE title = 'The Great Gatsby';
+// books not written by Dan Brown
+SELECT * FROM library WHERE author <> 'Dan Brown';
